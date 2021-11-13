7c12cf9c04935afd4382a1aa9a76f5e43321a8f7
Fabiano Parente <fabiano@nunes.me>
2021-11-12 23:34:32 -0300

    implementa prettier

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   parserOptions: {
     requireConfigFile: false,
   },
@@ -7,30 +7,30 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  plugins: ["react-hooks", "jquery"],
+  plugins: ['react-hooks', 'jquery'],
   extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "prettier",
-    "plugin:jquery/deprecated",
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:jquery/deprecated',
   ],
   rules: {
-    "react/prop-types": [0],
-    "import/prefer-default-export": [0], // essa regra é irritante
-    "jsx-a11y/media-has-caption": [0],
-    "no-param-reassign": [
-      "error",
-      { props: true, ignorePropertyModificationsFor: ["draft", "acc"] },
+    'react/prop-types': [0],
+    'import/prefer-default-export': [0], // essa regra é irritante
+    'jsx-a11y/media-has-caption': [0],
+    'no-param-reassign': [
+      'error',
+      { props: true, ignorePropertyModificationsFor: ['draft', 'acc'] },
     ],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       alias: {
-        map: [["@", "./src"]],
-        extensions: [".js", ".jsx"],
+        map: [['@', './src']],
+        extensions: ['.js', '.jsx'],
       },
     },
   },
-};
+}
