2d9549e31417b52bdc0ee5a79879c811b18d5d54
Fabiano Parente <fabiano@nunes.me>
2021-11-18 20:05:20 -0300

    completa configuração do typescript

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ module.exports = {
     'import/resolver': {
       alias: {
         map: [['@', './src']],
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
@@ -47,18 +47,28 @@ module.exports = {
       plugins: ['@typescript-eslint/eslint-plugin'],
       extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
+        // permite importar types, que geralmente são instalados como devDependencies
         'import/no-extraneous-dependencies': [
           'error',
           { devDependencies: true },
         ],
+
+        // permite importar arquivos (jsx?|tsx?) sem precisar da extensão
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          { ts: 'never', tsx: 'never', js: 'never', jsx: 'never' },
+        ],
+
+        // permite JSX em arquivos .tsx
+        'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+
+        // afrouxa algumas regras específicas do TypeScript
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-      },
-      settings: {
-        'import/resolver': {
-          typescript: {},
-        },
+
+        // 'react/react-in-jsx-scope': 'off',
       },
     },
   ],
