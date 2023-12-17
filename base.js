660c1e5c278104fda99a3487e00595eee8e95a9b
Fabiano Parente <fabiano@nunes.me>
2023-12-17 12:17:33 -0300

    ignora node_modules e dist

--- base.js
+++ base.js
@@ -15,6 +15,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
+  ignorePatterns: ['dist', 'node_modules'],
   rules: {
     'react/prop-types': 'off',
     'react/require-default-props': 'off',
